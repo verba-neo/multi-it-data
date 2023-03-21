0b9cab2ae14ed5280c28e491eb5a2cb79ede367f
edu-neo <verba.it.corp@gmail.com>
2023-03-21 15:30:21 +0900

    0321

--- 03_django/00_INTRO/utilities/views.py
+++ 03_django/00_INTRO/utilities/views.py
@@ -16,10 +16,55 @@ def get_lotto(request):
 def this_week(request):
     URL = 'https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=1059'
     res = requests.get(URL).json()
-    print(res)
-    return render(request, 'utilities/this_week.html')
+    real_numbers = []
+
+    for k, v in res.items():
+        if 'drwtNo' in k:
+            real_numbers.append(v)
+
+    # for i in range(1, 7):
+    #     real_numbers.append(res[f'drwtNo{i}'])
+
+    real_numbers.sort()
+    context = {
+        'real_numbers': real_numbers,
+        'bonus_number': res['bnusNo'],
+    }
+    return render(request, 'utilities/this_week.html', context)
 
 
 def check_lotto(request):
+    URL = 'https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=1059'
+    res = requests.get(URL).json()
+    real_numbers = []
 
-    return render(request, 'utilities/check_lotto.html')
+    for k, v in res.items():
+        if 'drwtNo' in k:
+            real_numbers.append(v)
+    bonus_number = res['bnusNo']
+
+    lucky_numbers = random.sample(range(1, 46), 6)
+
+    match_count = len(set(lucky_numbers) & set(real_numbers))
+
+    if match_count == 6:
+        result = '1등'
+    elif match_count == 5 and bonus_number in lucky_numbers:
+        result = '2등'
+    elif match_count == 5:
+        result = '3등'
+    elif match_count == 4:
+        result = '4등'
+    elif match_count == 3:
+        result = '5등'
+    else:
+        result = ':('
+
+    context = {
+        'result': result,
+        'real_numbers': real_numbers,
+        'bonus_number': bonus_number,
+        'lucky_numbers': lucky_numbers,
+    }    
+    
+    return render(request, 'utilities/check_lotto.html', context)
