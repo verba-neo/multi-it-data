641624c22c5100045cd72bcbb6a785ef473febcd
edu-neo <verba.it.corp@gmail.com>
2023-03-22 16:01:59 +0900

    0322

--- 03_django/01_USER_INPUT/myapp/urls.py
+++ 03_django/01_USER_INPUT/myapp/urls.py
@@ -4,8 +4,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # myapp/hello/neo/ => Variable Routing
+    path('hello/<str:name>/', views.hello, name='hello'),
     # myapp/ping/
-    path('ping/', views.ping),
+    path('ping/', views.ping, name='ping'),
     # myapp/pong/
-    path('pong/', views.pong),
+    path('pong/', views.pong, name='pong'),
+    # myapp/lotto_in/
+    path('lotto_in/', views.lotto_in, name='lotto_in'),
+    # myapp/lotto_out/
+    path('lotto_out/', views.lotto_out, name='lotto_out'),
 ]
--- 03_django/01_USER_INPUT/myapp/views.py
+++ 03_django/01_USER_INPUT/myapp/views.py
@@ -1,15 +1,50 @@
+import requests
 from django.shortcuts import render
 
-# myapp/ping/
+
+# name => Variable Routing
+def hello(request, name):
+    context = {
+        'name': name,
+    }
+    return render(request, 'myapp/hello.html', context)
+
+
 def ping(request):
     return render(request, 'myapp/ping.html')
 
 
 def pong(request):
-    # 사용자 입력은 
+    # POST 방식
+    # 1. <form method="POST">
+    # 2. <form> > {% csrf_token %}
+    # 3. view => request.POST
     context = {
-        'name': request.GET['myname'],
-        'age': request.GET['age'],
-        'mbti': request.GET['mbti'],
+        'name': request.POST['myname'],
+        'age': request.POST['age'],
+        'mbti': request.POST['mbti'],
     }
     return render(request, 'myapp/pong.html', context)
+
+
+def lotto_in(request):
+    return render(request, 'myapp/lotto_in.html')
+
+
+def lotto_out(request):
+    draw_no = request.GET['draw_no']
+    URL = f'https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo={draw_no}'
+    res = requests.get(URL).json()
+
+    numbers = [value for key, value in res.items() if 'drwtNo' in key]
+    numbers.sort()
+    bonus_no = res['bnusNo']
+
+    context = {
+        'draw_no': draw_no,
+        'numbers': numbers,
+        'bonus_no': bonus_no,
+    }
+
+    return render(request, 'myapp/lotto_out.html', context)
+
